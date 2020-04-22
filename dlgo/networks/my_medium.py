486206c12dfe4136742d36a535db1e2860c2819c
Nail Fayzullin <fayzulinnt@gmail.com>
2020-04-22 23:17:02 +0300

    added my_medium.py

---
+++ dlgo/networks/my_medium.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import
+
+# tag::small_network[]
+from keras.layers import BatchNormalization
+from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.convolutional import Conv2D, ZeroPadding2D
+
+
+def layers(input_shape):
+    return [
+        ZeroPadding2D(padding=2, input_shape=input_shape, data_format='channels_first'),
+        Conv2D(64, (13, 13), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        ZeroPadding2D(padding=2, input_shape=input_shape, data_format='channels_first'),
+        Conv2D(64, (9, 9), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        ZeroPadding2D(padding=2, input_shape=input_shape, data_format='channels_first'),
+        Conv2D(64, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        ZeroPadding2D(padding=2, input_shape=input_shape, data_format='channels_first'),
+        Conv2D(64, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        ZeroPadding2D(padding=2, data_format='channels_first'),  
+        Conv2D(48, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+        ZeroPadding2D(padding=2, data_format='channels_first'),
+        Conv2D(32, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+       BatchNormalization(),
+
+        ZeroPadding2D(padding=2, data_format='channels_first'),
+        Conv2D(32, (5, 5), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        ZeroPadding2D(padding=2, data_format='channels_first'),
+        Conv2D(32, (3, 3), data_format='channels_first'),
+        Activation('relu'),
+
+        BatchNormalization(),
+
+        Flatten(),
+        Dense(512),
+
+        Activation('relu'),
+    ]
+

5ce4a211e9d165c8d2cc05ec37b971ee2d37940a
Nail Fayzullin <fayzulinnt@gmail.com>
2020-09-17 19:42:09 +0300

    17.09.2020 18:50

--- dlgo/networks/my_network.py
+++ dlgo/networks/my_network.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
 
-# tag::small_network[]
 from keras.layers import BatchNormalization
 from keras.layers.core import Dense, Activation, Flatten
-from keras.layers.convolutional import Conv2D, ZeroPadding2D
+from keras.layers.convolutional import Conv2D
 
 
 def layers(input_shape):
     return [
-        Conv2D(192, (5, 5), padding='same', data_format='channels_first'),
+        Conv2D(192, (5, 5), input_shape=input_shape, padding='same', data_format='channels_first'),
         Activation('relu'),
 
         BatchNormalization(),

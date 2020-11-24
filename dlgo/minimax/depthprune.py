e487855bd95956fcca324e0a2ebb468411692287
Nail Fayzullin <fayzulinnt@gmail.com>
2020-11-24 11:29:23 +0300

    24.11.2020 11:20

--- dlgo/minimax/depthprune.py
+++ dlgo/minimax/depthprune.py
@@ -20,7 +20,7 @@ def reverse_game_result(game_result):
 
 
 # tag::depth-prune[]
-def best_result(game_state, max_depth, eval_fn):
+def best_result(game_state, max_depth, max_width, eval_fn):
     if game_state.is_over():                               # <1>
         if game_state.winner() == game_state.next_player:  # <1>
             return MAX_SCORE                               # <1>
@@ -31,11 +31,11 @@ def best_result(game_state, max_depth, eval_fn):
         return eval_fn(game_state)                         # <2>
 
     best_so_far = MIN_SCORE
-    predict_moves = game_state.legal_moves()[:5]           # Nail
+    predict_moves = game_state.legal_moves()[:max_width]           # Nail
     for candidate_move in predict_moves:        # <3>      # Nail
         next_state = game_state.apply_move(candidate_move) # <4>
         opponent_best_result = best_result(                # <5>
-            next_state, max_depth - 1, eval_fn)            # <5>
+            next_state, max_depth - 1, max_width, eval_fn)            # <5>
         our_result = -1 * opponent_best_result             # <6>
         if our_result > best_so_far:                       # <7>
             best_so_far = our_result                       # <7>
@@ -51,18 +51,19 @@ class DepthPrunedAgent(Agent):
         self.max_depth = max_depth
         self.eval_fn = eval_fn
         self.predict_moves = 0
+        self.max_width = 7
 
     def select_move(self, game_state):
         best_moves = []
         best_score = None
-        predict_moves = game_state.legal_moves()[:5]     # Nail
+        predict_moves = game_state.legal_moves()[:self.max_width]     # Nail
         # Loop over all legal moves.
         for possible_move in predict_moves:              # Nail
             # Calculate the game state if we select this move.
             next_state = game_state.apply_move(possible_move)
             # Since our opponent plays next, figure out their best
             # possible outcome from there.
-            opponent_best_outcome = best_result(next_state, self.max_depth, self.eval_fn)
+            opponent_best_outcome = best_result(next_state, self.max_depth, self.max_width, self.eval_fn)
             # Our outcome is the opposite of our opponent's outcome.
             our_best_outcome = -1 * opponent_best_outcome
             if (not best_moves) or our_best_outcome > best_score:
